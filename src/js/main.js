e008bc780c7be88c89cfc4b510195b9ae66d1c81
Michał <michal.gzella01@gmail.com>
2022-12-21 21:50:43 +0100

    drinks2

---
+++ src/js/main.js
@@ -0,0 +1,112 @@
+const comment_one = document.querySelector('.comment_one')
+const recipe_one = document.querySelector(".recipe_one")
+const flex_one = document.querySelector(".flex_one")
+
+const comment_two = document.querySelector('.comment_two')
+const recipe_two = document.querySelector(".recipe_two")
+const flex_two = document.querySelector(".flex_two")
+
+const comment_three = document.querySelector('.comment_three')
+const recipe_three = document.querySelector(".recipe_three")
+const flex_three = document.querySelector(".flex_three")
+
+const comment_four = document.querySelector('.comment_four')
+const recipe_four = document.querySelector(".recipe_four")
+const flex_four = document.querySelector(".flex_four")
+
+const comment_five = document.querySelector('.comment_five')
+const recipe_five = document.querySelector(".recipe_five")
+const flex_five = document.querySelector(".flex_five")
+
+const comment_six = document.querySelector('.comment_six')
+const recipe_six = document.querySelector(".recipe_six")
+const flex_six = document.querySelector(".flex_six")
+
+const comment_seven = document.querySelector('.comment_seven')
+const recipe_seven = document.querySelector(".recipe_seven")
+const flex_seven = document.querySelector(".flex_seven")
+
+const comment_eight = document.querySelector('.comment_eight')
+const recipe_eight = document.querySelector(".recipe_eight")
+const flex_eight = document.querySelector(".flex_eight")
+
+const comment_nine = document.querySelector('.comment_nine')
+const recipe_nine = document.querySelector(".recipe_nine")
+const flex_nine = document.querySelector(".flex_nine")
+
+const comment_ten = document.querySelector('.comment_ten')
+const recipe_ten = document.querySelector(".recipe_ten")
+const flex_ten = document.querySelector(".flex_ten")
+
+const comment_eleven = document.querySelector('.comment_eleven')
+const comment_twelve = document.querySelector('.comment_twelve')
+const recipe_eleven = document.querySelector(".recipe_eleven")
+const flex_eleven = document.querySelector(".flex_eleven")
+
+const turn_on = () => {
+    recipe_one.classList.toggle('turn_on');
+    flex_one.classList.toggle('turn_off')
+}
+
+const turn_on_two = () => {
+    recipe_two.classList.toggle('turn_on');
+    flex_two.classList.toggle('turn_off')
+}
+
+const turn_on_three = () => {
+    recipe_three.classList.toggle('turn_on');
+    flex_three.classList.toggle('turn_off')
+}
+
+const turn_on_four = () => {
+    recipe_four.classList.toggle('turn_on');
+    flex_four.classList.toggle('turn_off')
+}
+
+const turn_on_five = () => {
+    recipe_five.classList.toggle('turn_on');
+    flex_five.classList.toggle('turn_off')
+}
+
+const turn_on_six = () => {
+    recipe_six.classList.toggle('turn_on');
+    flex_six.classList.toggle('turn_off')
+}
+
+const turn_on_seven = () => {
+    recipe_seven.classList.toggle('turn_on');
+    flex_seven.classList.toggle('turn_off')
+}
+
+const turn_on_eight = () => {
+    recipe_eight.classList.toggle('turn_on');
+    flex_eight.classList.toggle('turn_off')
+}
+
+const turn_on_nine = () => {
+    recipe_nine.classList.toggle('turn_on');
+    flex_nine.classList.toggle('turn_off')
+}
+
+const turn_on_ten = () => {
+    recipe_ten.classList.toggle('turn_on');
+    flex_ten.classList.toggle('turn_off')
+}
+
+const turn_on_eleven = () => {
+    recipe_eleven.classList.toggle('turn_on');
+    flex_eleven.classList.toggle('turn_off');
+}
+
+comment_one.addEventListener('click', turn_on)
+comment_two.addEventListener('click', turn_on_two)
+comment_three.addEventListener('click', turn_on_three)
+comment_four.addEventListener('click', turn_on_four)
+comment_five.addEventListener('click', turn_on_five)
+comment_six.addEventListener('click', turn_on_six)
+comment_seven.addEventListener('click', turn_on_seven)
+comment_eight.addEventListener('click', turn_on_eight)
+comment_nine.addEventListener('click', turn_on_nine)
+comment_ten.addEventListener('click', turn_on_ten)
+comment_eleven.addEventListener('click', turn_on_eleven)
+comment_twelve.addEventListener('click', turn_on_eleven)
